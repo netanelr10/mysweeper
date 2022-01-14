e7ae4406e6f9c77a224d02d1143676a6291eafe1
אורח <@DESKTOP-2TQHGOH>
2022-01-14 16:01:10 +0200

    fix

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -10,13 +12,25 @@
 public class NewMain {
 
     public static void main(String[] args) {
-
-        int minesCount = 5;  //how many bombs.
-        int rowCount = 5;    //row of the game borad(array[][])
-        int columnsCount = 5; //
-
-        Borde g1 = new Borde(minesCount, rowCount , columnsCount);
-        g1.print();
+        Scanner sc = new Scanner(System.in);
+        int minesCount;  //how many bombs.
+        int rowCount;    //row of the game borad(array[][])
+        int columnsCount; //
+        int row;
+        int col;
+       
+            rowCount = sc.nextInt();
+            columnsCount = sc.nextInt();
+            minesCount = sc.nextInt();
+            Borde g1 = new Borde(minesCount, rowCount, columnsCount);
+            g1.print();
+     
+            while (g1.getGameAlive()) {
+                row = sc.nextInt();
+                col = sc.nextInt();
+                g1.open(row, col);
+            }
+   
+        }
 
     }
-}
